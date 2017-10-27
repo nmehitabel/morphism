f5d80cb5eda10abd9aaf5ed1189365790d5369dd
Gavin Casey <gavin.casey@ontrackretail.co.uk>
2017-10-27 11:30:43 +0100

    parametric polymorhism version

---
+++ src/main/scala/io/mehitabel/morphism/parametric/Pexp.scala
@@ -0,0 +1,10 @@
+package io.mehitabel.morphism
+package parametric
+
+sealed trait Exp[A]
+final case class IntValue[A](v: Int) extends Exp[A]
+final case class DecValue[A](v: Double) extends Exp[A]
+final case class Sum[A](exp1: A, exp2: A) extends Exp[A]
+final case class Multiply[A](exp1: A, exp2: A) extends Exp[A]
+final case class Divide[A](exp1: A, exp2: A) extends Exp[A]
+final case class Square[A](exp: A) extends Exp[A]
